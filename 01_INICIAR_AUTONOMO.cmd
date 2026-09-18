63e78a518d609894ca8ca1d3dbc32d2d1c8d26b3
Diw87 <wherbethjk@hotmail.com>
2026-09-18 10:38:23 -0300

    Adiciona 01_INICIAR_AUTONOMO.cmd da v0.2

---
+++ 01_INICIAR_AUTONOMO.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+set "dill_ps=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
+if exist "%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\powershell.exe" set "dill_ps=%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\powershell.exe"
+if not exist "%dill_ps%" (
+  echo Windows PowerShell nao foi encontrado neste computador.
+  pause
+  exit /b 1
+)
+"%dill_ps%" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0src\Start.ps1" -Mode Repair
+if errorlevel 1 pause
+endlocal
